7264d39d94ba771d036f7847131a1b6076ad8245
Sadik Elahi <selahi5@my.centennialcollege.ca>
2026-02-07 17:31:20 -0500

    Initial ASP.NET Core Hello World app

---
+++ Comp367-lab1/Program.cs
@@ -0,0 +1,15 @@
+namespace Comp367_lab1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
